4076b00cb13a319f30f9d7ee7555c14cff9bf0a5
Sadeq <mhmd01qk@gmail.com>
2020-11-22 08:00:40 +0330

    test of class in JS

---
+++ js/app.js
@@ -0,0 +1,44 @@
+
+
+// variables
+const form = document.querySelector('#request-quote')
+const html = new HTMLUI()
+
+
+
+// eventlisteners
+eventListeners()
+function eventListeners(){
+// make option tag for select
+document.addEventListener('DOMContentLoaded', function(){
+    // display the <option>
+    html.displayYears()
+})
+
+// submit form when click
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    // read value from the form
+    const make = document.getElementById('make').value
+    const year = document.getElementById('year').value
+    const level = document.querySelector('input[name="level"]:checked').value
+
+    // check the value of fields are correct
+    if (make === '' || year === '' || level === '') {
+        html.displayError('لطفا همه مقادیر به درستی وارد شود')
+    } else {
+        let resultDiv = document.querySelector('#result div')
+        if (resultDiv !== null) {
+            resultDiv.remove()
+        }
+        const insurance = new Insurance(make , year , level)
+        const price = insurance.calculatePrice(insurance)
+        html.showResult(price , insurance)
+    }
+})
+}
+
+
+
+
+
